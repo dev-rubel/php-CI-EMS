9c739d7a93206f021205b25e40dc740fdb4b0bfd
dpirubel <dpirubel@hotmail.com>
2018-01-26 11:25:48 +0600

    24-1-18/Home

--- application/views/backend/admin/subject.php
+++ application/views/backend/admin/subject.php
@@ -27,7 +27,7 @@
                 		<tr>
                     		<th><div><?php echo get_phrase('class');?></div></th>
                             <th><div><?php echo get_phrase('subject_name');?></div></th>
-                    		<th><div><?php echo get_phrase('mark');?></div></th>
+                    		<th><div><?php echo get_phrase('mark_distribution');?></div></th>
                     		<th><div><?php echo get_phrase('teacher');?></div></th>
                     		<th><div><?php echo get_phrase('options');?></div></th>
 						</tr>
@@ -41,7 +41,14 @@
                         <tr id="subject<?php echo $row['subject_id'];?>">
 							<td><?php echo $this->crud_model->get_type_name_by_id('class',$row['class_id']);?></td>
                             <td><?php echo count($joinOrNot)>1?$row['name'].' | <b>Join</b>':$row['name'].'<b> | '.ucfirst($row['subject_category']).'</b>'; echo '<b> | '.ucwords(notEmpty($groupName)).'</b>';?></td>
-							<td><?php echo count($joinOrNot)>1?$row['subject_mark'].' | <b>'.($joinOrNot[0]['subject_mark']+$joinOrNot[1]['subject_mark']).'</b>':$row['subject_mark']; ?></td>
+                            <td><?php 
+                                $subject_marks = explode('|', $row['subject_marks']);
+                                if(count($joinOrNot)>1) {                                    
+                                    echo 'MT: '.$subject_marks[0].'| CQ: '.$subject_marks[1].'| MCQ: '.$subject_marks[2].'| PR: '.$subject_marks[3].'| <b>Total: '.($joinOrNot[0]['total_mark']+$joinOrNot[1]['total_mark']).'</b>';
+                                } else {
+                                    echo 'MT: '.$subject_marks[0].'| CQ: '.$subject_marks[1].'| MCQ: '.$subject_marks[2].'| PR: '.$subject_marks[3].'| <b>Total: '.($joinOrNot[0]['total_mark']+$joinOrNot[1]['total_mark']).'</b>';
+                                } ?>
+                            </td>
 							<td><?php echo $this->crud_model->get_type_name_by_id('teacher',$row['teacher_id']);?></td>
 							<td>
                             <div class="btn-group">
@@ -52,11 +59,11 @@
                                     
                                     <!-- EDITING LINK -->
                                     <li>
-                                        <a href="#" onclick="editSubject('<?php echo $row['subject_id'];?>')">
+                                        <a href="#" onclick="editSubject('<?php echo $row['subject_id'];?>','<?php echo $row['class_id']; ?>')">
                                             <i class="entypo-pencil"></i>
                                                 <?php echo get_phrase('edit');?>
-                                            </a>
-                                                    </li>
+                                        </a>
+                                    </li>
                                     <li class="divider"></li>
                                     
                                     <!-- DELETION LINK -->
@@ -64,8 +71,8 @@
                                         <a href="#" onclick="confDelete('admin','ajax_delete_subject','<?php echo $row['subject_id'];?>','subject<?php echo $row['subject_id'];?>')">
                                             <i class="entypo-trash"></i>
                                                 <?php echo get_phrase('delete');?>
-                                            </a>
-                                                    </li>
+                                        </a>
+                                    </li>
                                 </ul>
                             </div>
         					</td>
@@ -73,6 +80,7 @@
                         <?php endforeach;?>
                     </tbody>
                 </table>
+
                 </div>
 			</div>
             <!----TABLE LISTING ENDS--->
@@ -107,8 +115,9 @@
                                         <option value="optional">Optional Subject</option>   
                                         <?php endif;?>    
                                         <?php 
-                                            $countJoin = $this->db->get_where('subject',array('class_id'=>$class_id,'subject_category'=>'main'))->result_array();
-                                            if(count($countJoin) > 0):
+                                            $countMain = $this->db->get_where('subject',array('class_id'=>$class_id,'subject_category'=>'main'))->result_array();
+                                            $countJoin = $this->db->get_where('subject',array('class_id'=>$class_id,'subject_category'=>'join'))->result_array();
+                                            if(count($countMain) > 0 && count($countJoin) <= 2):
                                         ?>                                
                                             <option value="join">Join Subject</option>
                                                 
@@ -120,7 +129,7 @@
                                 <label class="col-sm-3 control-label"><?php echo get_phrase('join_subject_list');?></label>
                                 <div class="col-sm-5">
                                     <select class="form-control" name="join_subject_code" id="join_subject_holder">
-                                        
+                                        <option value="">Select One</option>
                                     </select>
                                 </div>
                             </div>
@@ -128,15 +137,33 @@
                                 <label class="col-sm-3 control-label"><?php echo get_phrase('group_subject_name');?></label>
                                 <div class="col-sm-5">
                                     <select class="form-control" name="group_subject_name" id="group_subject_holder">
-                                        
+                                        <option value="">Select One</option>
                                     </select>
                                 </div>
                             </div>
+                            
                             <div class="form-group">
-                                <label class="col-sm-3 control-label"><?php echo get_phrase('subject_mark');?></label>
-                                <div class="col-sm-5">
-                                    <input type="number" class="form-control" name="subject_mark" data-validate="required" data-message-required="<?php echo get_phrase('value_required');?>"/>
+                                <label class="col-sm-3 control-label"><?php echo get_phrase('mark_distribution');?></label>
+                                <div class="col-sm-2">
+                                    <input type="number" class="form-control markSum" name="mt_mark" data-validate="required" data-message-required="<?php echo get_phrase('value_required');?>" placeholder="MT Mark"/>
                                 </div>
+                                <div class="col-sm-2">
+                                    <input type="number" class="form-control markSum" name="cq_mark" data-validate="required" data-message-required="<?php echo get_phrase('value_required');?>" placeholder="CQ Mark"/>
+                                </div>
+                                <div class="col-sm-2">
+                                    <input type="number" class="form-control markSum" name="mcq_mark" data-validate="required" data-message-required="<?php echo get_phrase('value_required');?>" placeholder="MCQ Mark"/>
+                                </div>
+                                <div class="col-sm-2">
+                                    <input type="number" class="form-control markSum" name="pr_mark" data-validate="required" data-message-required="<?php echo get_phrase('value_required');?>" placeholder="PR Mark"/>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-3 control-label"><?php echo get_phrase('total_mark');?></label>
+                                <div class="col-sm-5">
+                                    <input type="number" class="form-control markTotal" name="total_mark" data-validate="required" data-message-required="<?php echo get_phrase('value_required');?>" readonly/>
+                                </div>
+                                <h4 style="color: red;" class="invalidMark">Invalid Mark</h4>
                             </div>
                             
                             <input type="hidden" name="subject_code" value="<?php echo substr(md5(rand(0, 1000000)), 0, 5);?>">
@@ -160,10 +187,10 @@
                             </div>
                         </div>
                         <div class="form-group">
-                              <div class="col-sm-offset-3 col-sm-5">
-                                  <button type="submit" class="btn btn-info"><?php echo get_phrase('add_subject');?></button>
-                              </div>
-						   </div>
+                            <div class="col-sm-offset-3 col-sm-5">
+                                <button type="submit" class="btn btn-info markSaveButton"><?php echo get_phrase('add_subject');?></button>
+                            </div>
+                        </div>
                     </form>                
                 </div>                
 			</div>
@@ -176,8 +203,11 @@
 
 <!-----  DATA TABLE EXPORT CONFIGURATIONS ---->                      
 <script type="text/javascript">
+    $('#join_subject_holder').attr('disabled','disabled');
     $('#joinSubject').hide();
+    $('#group_subject_holder').attr('disabled','disabled');
     $('#groupSubject').hide();
+
 	jQuery(document).ready(function($)
 	{
 		var datatable = $("#table_export").dataTable();
@@ -195,19 +225,20 @@
                 url: '<?php echo base_url(); ?>index.php?admin/get_join_subject_info/' + this.class_id,
                 success: function (response)
                 {   
-
                     if(response){
                         $('#joinSubject').show();
                         $('#join_subject_holder').empty();
                         var jsonvalue =  JSON.parse(response);
+                        $('#join_subject_holder').append('<option value="">Select One</option>');
                         $.each(jsonvalue, function(key, value){
                             var div_data="<option value="+value.subject_code+">"+value.name+"</option>";
                             $(div_data).appendTo('#join_subject_holder'); 
-                        })
+                        });
                     }else{
                         $('#joinSubject').show();                        
                         $('#join_subject_holder').append('<option value="">No Main Subject Found</option>');
                     }
+                    $('#group_subject_holder').attr('disabled','disabled');
                 }
             });
 
@@ -221,7 +252,7 @@
                         $('#groupSubject').show();
                         $('#group_subject_holder').empty();
                         var jsonvalue =  JSON.parse(response);
-
+                        $('#group_subject_holder').append('<option value="">Select One</option>');
                         $.each(jsonvalue, function(key, value){
                             var div_data="<option value="+value.group_id+">"+titleCase(value.name)+"</option>"; 
                             $(div_data).appendTo('#group_subject_holder'); 
@@ -230,12 +261,15 @@
                         $('#groupSubject').show();
                         $('#group_subject_holder').append('<option value="">No Group Found</option>');
                     }
+                    $('#join_subject_holder').attr('disabled','disabled');
                 }
             });
 
         }else{
             $('#groupSubject').hide();
+            $('#group_subject_holder').attr('disabled','disabled');
             $('#joinSubject').hide();
+            $('#join_subject_holder').attr('disabled','disabled');
         }
         
     }
@@ -256,8 +290,25 @@
 
 <script>
 
-$(document).ready(function() {
+// CALCULATE TOTAL MARK AND SET INTO SUBJECT MARK INPUT FIELD
+$(".invalidMark").hide();
+$(document).on("change", ".markSum", function() {
+    var sum = 0;
+    $(".markSum").each(function(){
+        sum += +$(this).val();
+    });
+    if(sum > 100) {
+        $(".invalidMark").show();
+        $(".markSaveButton").attr('disabled','disabled');
+    } else {
+        $(".invalidMark").hide();
+        $(".markSaveButton").removeAttr('disabled','disabled');
+    }
+    $(".markTotal").val(sum);
+});
 
+$(document).ready(function() {
+  
     $('#createSubject').ajaxForm({ 
         beforeSend: function() {                
                 $('#loading2').show();
@@ -270,7 +321,7 @@ $(document).ready(function() {
                 toastr.error(jData.msg);
             } else {
                 toastr.success(jData.msg);  
-                $( "#subjectList" ).html( jData.html );
+                $("#subjectList").html( jData.html );
                 $("#table_export").dataTable();
                 $('#createSubject').resetForm();               
             }   
@@ -279,31 +330,27 @@ $(document).ready(function() {
             $('#overlayDiv').fadeOut('slow');                   
         }
     });
-    
-      
-
-
 });
 
-function editSubject(subjectID)
-    {
-        $.ajax({
-            type: 'GET',
-            url: '<?php echo base_url();?>index.php?admin/ajax_edit_subject/'+subjectID,
-            beforeSend: function(){
-                $('#loading2').show();
-                $('#overlayDiv').show();
-            },
-            success: function(data){
-                var jData = JSON.parse(data); 
-                
-                toastr.success(jData.msg);  
-                $( "#editSubjectHolder" ).html( jData.html );
-                $('body,html').animate({scrollTop:350},800);         
-                $('#loading2').fadeOut('slow');
-                $('#overlayDiv').fadeOut('slow');
-            }
-        });
-    }
+function editSubject(subjectID, classID)
+{
+    $.ajax({
+        type: 'GET',
+        url: '<?php echo base_url();?>index.php?admin/ajax_edit_subject/'+subjectID+'/'+classID,
+        beforeSend: function(){
+            $('#loading2').show();
+            $('#overlayDiv').show();
+        },
+        success: function(data){
+            var jData = JSON.parse(data); 
+            
+            toastr.success(jData.msg);  
+            $( "#editSubjectHolder" ).html( jData.html );
+            $('body,html').animate({scrollTop:350},800);         
+            $('#loading2').fadeOut('slow');
+            $('#overlayDiv').fadeOut('slow');
+        }
+    });
+}
 
 </script>
